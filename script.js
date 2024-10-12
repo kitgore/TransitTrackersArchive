245d75f744edeafb5c418de0da058e1bba8971c7
kitgore <bengriepp@gmail.com>
2024-10-11 17:41:32 -0700

    added all sections, fixed smooth scroll

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.onload = function() {
+    initSmoothScroll();
+};
+
+function initSmoothScroll() {
     const navLinks = document.querySelectorAll('.nav-links a, .logo a');
     const navbarHeight = document.querySelector('.navbar').offsetHeight;
 
     navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
-            e.preventDefault();
+            e.preventDefault(); // Prevent any default action
+            const targetId = this.getAttribute('data-scroll-to');
 
-            const targetId = this.getAttribute('href');
-            const targetSection = document.querySelector(targetId);
-
-            if (targetId === '#top') {
+            if (targetId === 'top') {
                 // Scroll to the top of the page
                 window.scrollTo({
-                  top: 0,
-                  behavior: 'smooth'
-                });
-              }
-
-            else if (targetSection) {
-                const targetPosition = targetSection.getBoundingClientRect().top + window.pageYOffset - navbarHeight + 20;
-                //Scroll to target position
-                window.scrollTo({
-                    top: targetPosition,
+                    top: 0,
                     behavior: 'smooth'
                 });
+            } else {
+                const targetElement = document.getElementById(targetId);
+                if (targetElement) {
+                    const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - navbarHeight + 20;
+                    
+                    // Scroll to target position
+                    window.scrollTo({
+                        top: targetPosition,
+                        behavior: 'smooth'
+                    });
+                }
             }
+
+            // Update the URL without navigating
+            history.pushState(null, '', targetId === 'top' ? '#' : `#${targetId}`);
         });
     });
-});
+}
